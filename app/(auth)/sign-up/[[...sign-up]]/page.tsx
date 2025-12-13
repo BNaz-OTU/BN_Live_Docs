20c64faf85432bd18f6d70d9c6593355f0a5ab9a
BNaz-OTU <bilal.nazar@ontariotechu.net>
2025-12-12 21:42:20 -0500

    Fixed security issue related to React2Shell and fixed my Sign Up page

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,12 +1,12 @@
-import { Signup } from '@clerk/nextjs'
-import React from 'react'
+import { SignUp } from "@clerk/nextjs";
+import React from "react";
 
 const SignupPage = () => {
-    return (
-        <main className='auth-page'>
-            <Signup />
-        </main>
-    )
-}
+  return (
+    <main className="auth-page">
+      <SignUp />
+    </main>
+  );
+};
 
-export default SignupPage
+export default SignupPage;
